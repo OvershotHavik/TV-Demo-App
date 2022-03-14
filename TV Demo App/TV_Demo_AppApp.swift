5223f4b70cd54adefa200bac7f2fbd437f12e014
OvershotHavik <plavetzky@gmail.com>
2022-03-14 16:28:55 -0400

    Initial Commit

---
+++ TV Demo App/TV_Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TV_Demo_AppApp.swift
+//  TV Demo App
+//
+//  Created by Steve Plavetzky on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct TV_Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
